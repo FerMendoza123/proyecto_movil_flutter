c3ce67704b54b365e717788eed4631cf70ba0846
FerMendoza123 <caifanes_10@live.com.mx>
2022-11-26 00:34:10 -0600

    customButton.dart added in order to replace Flutter Flow Button

---
+++ lib/customButton.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget{
+  final double width;
+  final double height;
+  final String text;
+  final Color color;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final double elevation;
+
+  const CustomButton ({Key?key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.color,
+    required this.fontSize,
+    required this.fontWeight,
+    required this.elevation,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      height: height,
+      width: width,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: color,
+            elevation: elevation,
+          ),
+          onPressed: ()=>{},
+          child: Text(text,
+            style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+            ),
+          ),
+      ),
+    );
+  }
+}
+/*FFButtonWidget(                                                     *//*Sustituir por otro widget de boton*//*
+  onPressed: () {                                                         //
+    print('Button pressed ...');                                          //
+  },                                                                      //
+  text: 'Registrar',                                                      //
+  options: FFButtonOptions(                                               //
+    width: 270,                                                           //
+    height: 50,                                                           //
+    color: Color(0xFF4B39EF),                                             //
+    textStyle:                                                            //
+    FlutterFlowTheme.of(context).subtitle1.override(                       *//*Sustituir por otra clase de estilos*//*
+      fontFamily: 'Outfit',                                               //
+      color: Colors.white,                                                //
+      fontSize: 18,                                                       //
+      fontWeight: FontWeight.normal,                                      //
+    ),
+    elevation: 3,
+    borderSide: BorderSide(
+      color: Colors.transparent,
+      width: 1,
+    ),
+  ),
+),*/
